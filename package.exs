4a71000c20f686572a358f58ac085664b4476557
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2014-02-06 23:59:33 -0600

    Updated keywords.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(name: "encore-beyond",
   description: "Metadata as a Service for Encore",
   version: "0.0.1",
-  keywords: ["LFE", "Lisp", "Library", "API", "REST"],
+  keywords: ["LFE", "Lisp", "Library", "API", "REST", "API", "HTTP", "Web"],
   maintainers: [[name: "Duncan McGreggor",
                  email: "duncan.mcgreggor@rackspace.com"],
                 [name: "Josh Schairbaum",
